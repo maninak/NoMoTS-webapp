a8c7282d1458bb4361cdf471f2205f4abe0a7627
Kostis Maninakis <kmaninak@gmail.com>
2017-06-28 17:24:46 +0300

    feat(play): programmatically show available actions depending on selected endpoint

--- src/pages/companies/companies.ts
+++ src/pages/companies/companies.ts
@@ -13,12 +13,32 @@ import 'rxjs/add/operator/toPromise';
 export class CompaniesPage {
   apiSchema: Object;
   endpoints: [string];
+  actions: [string];
 
   constructor(
       public navCtrl: NavController,
       public navParams: NavParams,
       public http: Http,
   ) {}
+
+  onEndpointChange(event: string): void {
+    let actionsObj: Object = this.apiSchema[event];
+
+    let isDirty: boolean = true;
+    // iterate through api request schema and configure actions
+    for (let action in actionsObj) {
+      if ((this.actions !== undefined) && isDirty) {
+        this.actions.length = 0; // empty array to erase possible old entries from older endpoint selection
+        isDirty = false;
+      }
+      if (this.actions === undefined) {
+        // if array is undefined then instantiate it
+        this.actions = [action.toUpperCase()];
+      } else {
+        this.actions.push(action.toUpperCase());
+      }
+    }
+  }
 
   private ionViewWillLoad(): void {
     this.loadApiSchema();
@@ -30,7 +50,7 @@ export class CompaniesPage {
       .then( (response: Object) => {
         this.apiSchema = JSON.parse(response['_body'].toString());
 
-        // iterate through api request schema and init endpoints
+        // iterate through api request schema and configure endpoints
         for (let endpoint in this.apiSchema) {
           if (this.endpoints === undefined) {
             // if array is undefined then instantiate it
