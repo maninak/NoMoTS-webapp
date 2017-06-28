2a0f120cebb48095186846846d83881f955f9c75
Kostis Maninakis <kmaninak@gmail.com>
2017-06-28 17:30:34 +0300

    style(play): move code around so as to be better readable

--- src/pages/companies/companies.ts
+++ src/pages/companies/companies.ts
@@ -21,25 +21,6 @@ export class CompaniesPage {
       public http: Http,
   ) {}
 
-  onEndpointChange(event: string): void {
-    let actionsObj: Object = this.apiSchema[event];
-
-    let isDirty: boolean = true;
-    // iterate through api request schema and configure actions
-    for (let action in actionsObj) {
-      if ((this.actions !== undefined) && isDirty) {
-        this.actions.length = 0; // empty array to erase possible old entries from older endpoint selection
-        isDirty = false;
-      }
-      if (this.actions === undefined) {
-        // if array is undefined then instantiate it
-        this.actions = [action.toUpperCase()];
-      } else {
-        this.actions.push(action.toUpperCase());
-      }
-    }
-  }
-
   private ionViewWillLoad(): void {
     this.loadApiSchema();
   }
@@ -60,6 +41,25 @@ export class CompaniesPage {
           }
         }
       });
+  }
+
+  private onEndpointChange(event: string): void {
+    let actionsObj: Object = this.apiSchema[event];
+
+    let isDirty: boolean = true;
+    // iterate through api request schema and configure actions
+    for (let action in actionsObj) {
+      if ((this.actions !== undefined) && isDirty) {
+        this.actions.length = 0; // empty array to erase possible old entries from older endpoint selection
+        isDirty = false;
+      }
+      if (this.actions === undefined) {
+        // if array is undefined then instantiate it
+        this.actions = [action.toUpperCase()];
+      } else {
+        this.actions.push(action.toUpperCase());
+      }
+    }
   }
 
 }
