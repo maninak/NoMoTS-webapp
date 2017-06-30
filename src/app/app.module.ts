5f825b00668b2e4a84ec534bc2d54e2802c3235f
Kostis Maninakis <kmaninak@gmail.com>
2017-06-30 07:16:06 +0300

    feat(play): show response and add style if success or error, style header, change colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { CompaniesPageModule } from './../pages/companies/companies.module';
-
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
+import { CompaniesPageModule } from './../pages/companies/companies.module';
 import { MyApp } from './app.component';
 
 
